6356ceabd563bc4381dd43ae805b56d61a6c69e2
Rex Herndon <herndonrex1@gmail.com>
2024-05-20 19:27:14 -0500

    added fake back-end

--- reminders-app/src/App.tsx
+++ reminders-app/src/App.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ReminderList from './components/ReminderList';
 import Reminder from './models/reminder';
+import reminderService from './services/reminder'
 
-const reminders: Reminder[] = [
-  { id: 1, title: 'Reminder 1' }
-]
 
 function App() {
+  // this code should only be focused on rendering the UI
+  // other code, like calling the API in local storage, should be implemented in other classes
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await reminderService.getReminders();
+    setReminders(reminders);
+  }
+
   return (
     <div className="App">
       <ReminderList items={reminders} />
